68a52b19442258872b067da8b48364d298b040df
tawanchaiii <tawanchai_champ@hotmail.com>
2022-08-02 09:12:29 +0700

    add cors

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const School = require("./models/school");
 const Product = require("./models/product");
 const routes = require("./routes/routes");
 const authRoutes = require("./routes/secure_routes");
+const cors = require('cors');
 const session = require('express-session');
 
 require("dotenv").config();
@@ -19,6 +20,7 @@ mongoose.connect(process.env.DB_URI, {
 
 app.use(express.json());
 app.use(routes);
+app.use(cors())
 app.use(session({ 
   resave: false,
   saveUninitialized: true,
