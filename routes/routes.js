09025b126add47dd5f2151653fc4c31e68bc9157
tawanchaiii <tawanchai_champ@hotmail.com>
2022-08-30 11:10:36 +0700

    fix check Active user from email

--- routes/routes.js
+++ routes/routes.js
@@ -4,23 +4,23 @@ const bcrypt = require("bcryptjs");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const UserModel = require("../models/user");
-const SchoolModel = require('../models/school');
-const AdminModel = require('../models/admin');
+const SchoolModel = require("../models/school");
+const AdminModel = require("../models/admin");
 const userService = require("../services/users");
 const { cloudinary } = require("../utils/cloudinary");
 const { sender } = require("../utils/mail");
-const verifyUser = require("../middleware/verifyUser")
+const verifyUser = require("../middleware/verifyUser");
 
 const SALT_WORK_FACTOR = process.env.SALT_WORK_FACTOR;
 require("dotenv").config();
 
 router.route("/register").post(async (req, res) => {
-  const { userId, password, confirmPassword, email, role, certificate_doc} =
+  const { userId, password, confirmPassword, email, role, certificate_doc } =
     req.body;
 
   if (password != confirmPassword)
     return res.status(400).send("Password is not same.");
-  if ((!userId, !password, !email, !role)){
+  if ((!userId, !password, !email, !role)) {
     console.log(userId, password, email, role);
     return res.status(400).send("Please enter all parameter.");
   }
@@ -34,12 +34,13 @@ router.route("/register").post(async (req, res) => {
   });
   const url_doc = uploadedRes.secure_url;
 
-  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let token = '';
+  const characters =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let token = "";
   for (let i = 0; i < 25; i++) {
     token += characters[Math.floor(Math.random() * characters.length)];
   }
-  console.log(token)
+  console.log(token);
   const data = {
     userId,
     email,
@@ -47,12 +48,12 @@ router.route("/register").post(async (req, res) => {
     password: hashPassword,
     certificate_doc: url_doc,
     status: "Pending",
-    confirmationCode:token
+    confirmationCode: token,
   };
-  
+
   const user = new UserModel(data);
   const _user = await user.save();
-  sender(data.email, data.userId, data.confirmationCode)
+  sender(data.email, data.userId, data.confirmationCode);
   return res.json({ success: true, data: _user });
 });
 
@@ -61,17 +62,15 @@ router.route("/login").post(async (req, res) => {
   const _user = await userService.getUserByUsername(userId);
   if (_user) {
     if (bcrypt.compareSync(password, _user.password)) {
-      if (req.body.status != "Active") {
-        return res.status(401).send({
-          message: "Pending Account. Please Verify Your Email!",
-        });
-      }
-      else {
-        const _userInfo = await userService.getUserWithoutPassword(_user._id);
+      const _userInfo = await userService.getUserWithoutPassword(_user._id);
+      if (_userInfo.status === "Active") {
         const token = jwt.sign(_userInfo, process.env.SECRET, {
           expiresIn: "1h",
         });
         return res.json({ success: true, token: token });
+      }
+      else {
+        return res.status(401).send("Email is not activated");
       }
     }
   } else if (!userId || !password) {
@@ -80,6 +79,6 @@ router.route("/login").post(async (req, res) => {
   return res.status(401).send("Email or password is not correct.");
 });
 
-router.get("/confirm/:confirmationCode",verifyUser)
+router.get("/confirm/:confirmationCode", verifyUser);
 
 module.exports = router;
