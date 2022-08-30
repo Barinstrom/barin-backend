a9cbfbebb788d0ab39092a38b5feceedfa23e8df
tawanchaiii <tawanchai_champ@hotmail.com>
2022-08-30 15:18:51 +0700

    merge with csvToDB

--- server.js
+++ server.js
@@ -5,37 +5,48 @@ const passport = require("passport");
 const School = require("./models/school");
 const routes = require("./routes/routes");
 const authRoutes = require("./routes/secure_routes");
-const port = process.env.PORT || 54321
-const cors = require('cors');
-const session = require('express-session');
+const csvToJson = require("./utils/csvToJson");
+const os = require("os");
+const multer = require("multer");
+const upload = multer({ dest: os.tmpdir() });
+const port = process.env.PORT || 54321;
+const cors = require("cors");
+const session = require("express-session");
 
 require("dotenv").config();
-require('./config/passport')(passport)
+require("./config/passport")(passport);
 
 mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true,
 });
 
-app.use(cors())
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 // unsecured routes
 app.use(routes);
 
-app.use(session({ 
-  resave: false,
-  saveUninitialized: true,
-  secret: process.env.SECRET 
-}));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SECRET,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 // '/auth' => secured route
-app.use(
-  "/auth",
-  passport.authenticate("jwt", { session: false }),
-  authRoutes
-);
-
+app.use("/auth", passport.authenticate("jwt", { session: false }), authRoutes);
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const file = req.file;
+  if (file) {
+    const payload = await csvToJson(file);
+    //use payload to do something else
+    res.json(payload);
+  } else {
+    res.status(401).send("file doesn't exist.");
+  }
+});
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
---
+++ utils/csvToJson.js
@@ -0,0 +1,26 @@
+const parse = require("csv-parse/sync").parse;
+const fs = require("fs");
+
+const csvToJson = async (file) => {
+  const data = fs.readFileSync(file.path);
+  try {
+    const result = parse(data);
+    console.log("result =", result);
+
+    const head = result[0];
+    const ans = [];
+    for (let i = 1; i < result.length; i++) {
+      let tmp = {};
+      for (let j = 0; j < result[0].length; j++) {
+        tmp[head[j]] = result[i][j];
+      }
+      ans.push(tmp);
+    }
+    console.log("ans= ", ans);
+    return ans;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+module.exports = csvToJson;
