5ebdcf19640eea4cbfd783afce0c55f3ebcc3dea
Paradorn-248 <paradorn248@gmail.com>
2022-09-12 16:49:07 +0700

    add deleteClubs

--- services/users/updateStudent.js
+++ services/users/updateStudent.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const updateStudent = async (req, res) => {
     await StudentModel.findOneAndUpdate({ userID: new mongoose.mongo.ObjectId(req.body.userID) }, { $set: req.body })
         .then(() => {
-            res.send("Edit student success.");
+            res.json({'success': true});
         })
         .catch((err) => {
             res.status(400).send(err);
--- services/users/updateTeacher.js
+++ services/users/updateTeacher.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const updateTeacher = async (req, res) => {
     await TeacherModel.findOneAndUpdate({ userID: new mongoose.mongo.ObjectId(req.body.userID) }, { $set: req.body })
         .then(() => {
-            res.send("Edit teacher success.");
+            res.json({'success': true});
         })
         .catch((err) => {
             res.status(400).send(err);
