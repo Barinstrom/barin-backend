aa93b4c6079d13aa70bfc5e96eb63b4142d79677
Kop <copter1144@hotmail.com>
2022-09-12 16:48:27 +0700

    add Club bit not check school

--- services/users/addClub.js
+++ services/users/addClub.js
@@ -1,40 +1,35 @@
 const clubModel = require("../../models/club");
+const teacherModel = require("../../models/teacher");
 
 const addClub = async (req, res) => {
-   const payload = req.body;
-   const teacherID = req.body.teacherID;
-   delete payload["teacherID"];
+   //check club name
+   if (await clubModel.findOne({ clubName: req.body.clubName }))
+      return res.status(400).send({ error: "Club name is already exists." });
 
-   const club = new clubModel(payload);
+   const payloadClub = req.body;
+   const teacherID = req.body.teacherID;
+   delete payloadClub["teacherID"];
+
+   //add new club
+   const club = new clubModel(payloadClub);
    const newClub = await club.save();
    const clubID = newClub._id;
 
-   //หา student เพื่อไป add reviewID in student
-   const studentID = req.body.studentID;
-   const student = await teacherModel.findOne({ userID: teacherID });
+   //หา teacher เพื่อไป add clubID in teacher
+   const teacher = await teacherModel.findOne({ userID: teacherID });
 
    // add clubID in teacher
-   const payloadTeacher = [...teacher.club, clubID];
-   await studentModel.updateOne(
-      { userID: studentID },
-      { $set: { reviews: payloadStudent } }
-   );
-
-   
-
-   // const club = new clubModel(payload);
-   // await club
-   //    .save()
-   //    .then(() => {
-   //       res.send({ success: true });
-   //    })
-   //    .catch((err) => {
-   //       if (clubModel.findOne({ clubName: req.body.clubName }))
-   //          return res
-   //             .status(400)
-   //             .send({ error: "Club name is already exists." });
-   //       res.status(400).send(err);
-   //    });
+   const payloadTeacher = [...teacher.clubs, clubID];
+   await teacherModel.updateOne(
+      { userID: teacherID },
+      { $set: { clubs: payloadTeacher } }
+   ).then(() => {
+      res.send({ success: true });
+   })
+   .catch((err) => {
+      res.status(400).send(err);
+   });
+      
 };
 
 module.exports = addClub;
