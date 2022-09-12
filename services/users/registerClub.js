99fd608c97d34fcc08946be987b6687841810204
S.Chayapon <chayapon.sa@ku.th>
2022-09-12 16:43:55 +0700

    fix dataModel

--- services/users/registerClub.js
+++ services/users/registerClub.js
@@ -1,8 +1,11 @@
-const clublModel = require("../../models/club");
 const studentModel = require("../../models/student");
+const schoolModel = require('../../models/school');
+const clublModel = require("../../models/club");
+const logsModel = require('../../models/registerLogs');
 
 const registerClub = async (req, res) => {
     //เช็คว่า schedule ของ school อยู่ใน registerDate มั้ย
+
     
     //เช็คว่า student ยังไม่ได้ลงทะเบียนในปีปัจจุบัน
 
