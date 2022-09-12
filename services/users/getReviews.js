3ddf97aba3401be000a5d3501bf0094f3c22b7b5
Kop <copter1144@hotmail.com>
2022-09-12 13:09:35 +0700

    add gorupID in reviews Model

---
+++ services/users/getReviews.js
@@ -0,0 +1,9 @@
+const clublModel = require("../../models/club");
+const reviewModel = require("../../models/review");
+
+const getReviews = async (req, res) => {
+   
+   
+};
+
+module.exports = getReviews;
