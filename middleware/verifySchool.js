e7da83029ae0ab81836aae2fef4070e3db3346f4
S.Chayapon <chayapon.sa@ku.th>
2022-09-12 13:25:51 +0700

    fix conflict

--- middleware/verifySchool.js
+++ middleware/verifySchool.js
@@ -1,35 +1,33 @@
-const schoolModel = require("../models/school");
+const schoolModel = require('../models/school');
 
-const verifySchool = async (req, res, next) => {
-   if (!req || !req.userInfo || !req.userInfo.schoolID)
-      return res.sendStatus(400);
+const verifySchool = async (req,res,next) => {
+    if(!req || !req.userInfo || !req.userInfo.school) return res.sendStatus(400);
 
-   /*try{
+    /*try{
         const school = await schoolModel.findOne({schoolID: req.params.school}).exec();
     }catch(err){
         console.log(err);
     }*/
-   console.log("params", req.params);
-   const school = await schoolModel
-      .findOne({ schoolID: req.params.school })
-      .lean()
-      .exec();
-   if (!school) {
-      // พิมพ์ route มั่ว
-      return res.status(400).send({ message: "this route is invalid" });
-   }
-   console.log(school);
-   if (school.schoolID !== req.userInfo.schoolID) {
-      console.log(req.userInfo);
-      // ถ้า school ใน token เป็น all => host ให้ผ่านไปได้เลย
-      if (req.userInfo.schoolID === "all") {
-         next();
-      }
-      // พยายามเข้า route ของโรงเรียนอื่น
-      return res.status(401).send("this is not yours school :(");
-   }
-   // หลุดมาถึงตรงนี้ => ok มีสิทธ์เข้าถึง
-   next();
-};
+    console.log('params',req.params);
+    // req.params = /:schoolID
+    const school = await schoolModel.findOne({schoolID: req.params.school}).lean().exec();
+    if(!school){
+        // พิมพ์ route มั่ว
+        return res.status(400).send({'message':'this route is invalid'});
+    }
+    console.log(school);
+    if(school.schoolID !== req.userInfo.school){
+        console.log(req.userInfo)
+        // ถ้า school ใน token เป็น all => host ให้ผ่านไปได้เลย
+        if(req.userInfo.school === 'all'){
+            req.userInfo.schoolID = req.params;
+            next();
+        }
+        // พยายามเข้า route ของโรงเรียนอื่น
+        return res.status(401).send('this is not yours school :(');
+    }
+    // หลุดมาถึงตรงนี้ => ok มีสิทธ์เข้าถึง
+    next();
+}
 
 module.exports = verifySchool;
