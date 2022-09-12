ce279339c90bcf935c178d5b0fd3082579bc3781
boss11887 <boss-boss2@hotmail.com>
2022-09-12 23:48:58 +0700

    clean paginate code + fix adminEdit.js show error message (not finishgit)

--- services/users/system_admin/getPendingSchool.js
+++ services/users/system_admin/getPendingSchool.js
@@ -4,11 +4,8 @@ const schoolModel = require("../../../models/school");
 const getPendingSchool = (req, res) => {
 
    let tmp = ""
-   if (!req.query.query){
-      tmp = ""
-   }else{
+   if (req.query.query)
       tmp = new RegExp("^" + req.query.query );
-   }
    const query = {
       schoolID: {$regex: tmp ,$options:'i'},
       status: "pending"
@@ -23,7 +20,7 @@ const getPendingSchool = (req, res) => {
          res.send(result);
       })
       .catch((err) => {
-         res.status(400).send("paginate error");
+         res.ustats(400).send("paginate error");
       });
 };
 
