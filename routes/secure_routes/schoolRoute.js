582a2fd00a7ba488de9468742c97671cd49f2492
Kop <copter1144@hotmail.com>
2022-09-12 13:58:04 +0700

    draft registerCLub

--- routes/secure_routes/schoolRoute.js
+++ routes/secure_routes/schoolRoute.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-<<<<<<< HEAD
 const verifyRole = require('../../middleware/verifyRole');
 const studentRoute = require("./student");
 const teacherRoute = require("./teacher");
@@ -11,10 +10,10 @@ router.get('/test',verifyRole('admin'),)
 router.use('/student',verifyRole('student'),studentRoute);
 router.use('/teacher',verifyRole('teacher'),teacherRoute);
 router.use('/admin',verifyRole('admin'),adminRoute);
-=======
-const verifyRole = require("../../middleware/verifyRole");
+
 const queryUser = require("../../services/users/queryUser");
 const addClub = require("../../services/users/addClub");
+const registerClub = require("../../services/users/registerClub");
 const addReview = require("../../services/users/addReview");
 const updateReview = require("../../services/users/updateReview");
 const getStudentOwnClubs = require("../../services/users/getStudentOwnClubs");
@@ -35,11 +34,11 @@ router.post("/add-student", verifyRole("host", "admin"), addStudent);
 router.post("/add-teacher", verifyRole("host", "admin"), addTeacher);
 router.post("/add-teachers", verifyRole("host", "admin"), addTeachers);
 router.post("/add-review",verifyRole("host", "admin", "teacher", "student"),addReview);
+router.post("/register-club",verifyRole("student"),registerClub);
 router.post("/update-review",verifyRole("host", "admin", "teacher", "student"),updateReview);
 router.post("/edit", verifyRole("host", "admin"), editSchool);
 router.get("/student/ownclub", verifyRole("student"), getStudentOwnClubs);
 router.get("/teacher/ownclubs", verifyRole("teacher"), getTeacherOwnClubs);
 router.get("/get-school", verifyRole("host", "admin"), querySchool);
->>>>>>> 3ddf97aba3401be000a5d3501bf0094f3c22b7b5
 
 module.exports = router;
