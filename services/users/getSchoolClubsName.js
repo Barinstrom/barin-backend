831dd963ab0fed095bafc8e161ae2c7d5236a77e
Kop <copter1144@hotmail.com>
2022-10-02 00:05:27 +0700

    edit getClubName

--- services/users/getSchoolClubsName.js
+++ services/users/getSchoolClubsName.js
@@ -3,12 +3,12 @@ const SchoolModel = require("../../models/school");
 //const { PaginationParameters } = require('mongoose-paginate-v2');
 
 const getSchoolClubsName = async (req, res) => {
-   const tmpClub = await clubModel.find({schoolID:req.userInfo.schoolID}).select({clubName:1, _id:0});
-   result = []
-   for(let i=0; i<tmpClub.length; i++){
-      result.push(tmpClub[i].clubName);
-   }
-   res.send(result);
+   const clubName = await clubModel.find({schoolID:req.userInfo.schoolID}).select({clubName:1});
+   // result = []
+   // for(let i=0; i<tmpClub.length; i++){
+   //    result.push(tmpClub[i].clubName);
+   // }
+   res.send(clubName);
 };
 
 module.exports = getSchoolClubsName;
