d3a1cbc16b4b7a3d09a3410deb060d6361ebea9d
boss11887 <boss-boss2@hotmail.com>
2022-10-05 20:54:36 +0700

    check

--- services/users/system_admin/schoolEditadmin.js
+++ services/users/system_admin/schoolEditadmin.js
@@ -1,12 +1,17 @@
 const schoolModel = require("../../../models/school");
+const { cloudinary } = require("../../utils/cloudinary");
 
-const editSchoolAdmin = (req, res) => {
+const editSchoolAdmin = async (req, res) => {
    const values = ({
       schoolName,
       urlLogo
    } = req.body);
-   console.log('------------------')
-   console.log(req.userInfo.schoolID)
+   //console.log('------------------')
+   //console.log(req.userInfo.schoolID)
+   const school = await SchoolModel.findOne({
+      schoolID: req.userInfo.schoolID,
+   });
+   console.log(school)
 
    schoolModel
       .findOneAndUpdate({ schoolID: req.userInfo.schoolID }, {$set: values})
@@ -23,3 +28,26 @@ const editSchoolAdmin = (req, res) => {
 };
 
 module.exports = editSchoolAdmin;
+
+/*
+const updateSchool = async (req, res) => {
+   const school = await SchoolModel.findOne({
+      schoolID: req.userInfo.schoolID,
+   });
+
+   if (schoolName) {
+      school.schoolName = schoolName;
+   }
+   if (logo) {
+      const uploadLogo = await cloudinary.uploader.upload(logo, {
+         upload_preset: "urlLogo",
+         public_id: req.userInfo.email,
+      });
+      const urlLogo = uploadLogo.secure_url;
+      school.urlLogo = urlLogo;
+   }
+   await school.save();
+   res.json({ success: "true" });
+};
+
+module.exports = updateSchool;*/
