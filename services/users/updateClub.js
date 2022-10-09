10a22353004039127ed83d9168bddb87926ba615
Paradorn-248 <paradorn248@gmail.com>
2022-10-09 15:17:43 +0700

    add cloudinary in updateClub

--- services/users/updateClub.js
+++ services/users/updateClub.js
@@ -6,28 +6,30 @@ const updateClub = async (req, res) => {
    const clubID = payload.clubID
    console.log(req.params.schoolID)
    delete payload["clubID"]
-
-   const uploadPic = await cloudinary.uploader.upload(req.body.urlPicture, {
-      upload_preset: "urlPicture",
-      public_id: req.userInfo.email,
-   });
-   const urlPicture = uploadPic.secure_url;
-   payload["urlPicture"] = urlPicture;
-
-
+   
+   if (req.body.urlPicture) {
+      const uploadPic = await cloudinary.uploader.upload(req.body.urlPicture, {
+         upload_preset: "urlPicture",
+         public_id: req.userInfo.email,
+      });
+      const urlPicture = uploadPic.secure_url;
+      payload["urlPicture"] = urlPicture;  
+   }
+   
    console.log(payload);
    // console.log(clubID);
    const _club = await ClubModel.findById(clubID)
-   if (!_club) {
+   if(!_club)
+   {
       return res.status(400).send("Invalid clubID")
    }
-   await ClubModel.findByIdAndUpdate(clubID, { $set: payload })
-      .then(() => {
-         res.send({ success: true });
-      })
-      .catch((err) => {
-         res.status(400).send(err);
-      });
+   await ClubModel.findByIdAndUpdate(clubID, {$set: payload})
+   .then(() => {
+      res.send({success: true});
+   })
+   .catch((err) => {
+      res.status(400).send(err);
+   });
 };
 
 module.exports = updateClub;
