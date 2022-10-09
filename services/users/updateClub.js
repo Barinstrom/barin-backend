c40811b767f23907abca95db5f55f21de4dea809
Paradorn-248 <paradorn248@gmail.com>
2022-10-09 15:14:55 +0700

    dd cloudinary in updateClub

--- services/users/updateClub.js
+++ services/users/updateClub.js
@@ -1,24 +1,33 @@
 const ClubModel = require("../../models/club");
+const { cloudinary } = require("../../utils/cloudinary");
 
 const updateClub = async (req, res) => {
    const payload = req.body;
    const clubID = payload.clubID
    console.log(req.params.schoolID)
    delete payload["clubID"]
-   // console.log(payload);
+
+   const uploadPic = await cloudinary.uploader.upload(req.body.urlPicture, {
+      upload_preset: "urlPicture",
+      public_id: req.userInfo.email,
+   });
+   const urlPicture = uploadPic.secure_url;
+   payload["urlPicture"] = urlPicture;
+
+
+   console.log(payload);
    // console.log(clubID);
    const _club = await ClubModel.findById(clubID)
-   if(!_club)
-   {
+   if (!_club) {
       return res.status(400).send("Invalid clubID")
    }
-   await ClubModel.findByIdAndUpdate(clubID, {$set: payload})
-   .then(() => {
-      res.send({success: true});
-   })
-   .catch((err) => {
-      res.status(400).send(err);
-   });
+   await ClubModel.findByIdAndUpdate(clubID, { $set: payload })
+      .then(() => {
+         res.send({ success: true });
+      })
+      .catch((err) => {
+         res.status(400).send(err);
+      });
 };
 
 module.exports = updateClub;
