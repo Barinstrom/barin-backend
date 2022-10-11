ca2780ab177574660240fe8c41f2a21d4e37ec67
tawanchaiii <tawanchai_champ@hotmail.com>
2022-10-11 21:27:07 +0700

    change response to boolean type <again>

--- services/users/unauth/activate.js
+++ services/users/unauth/activate.js
@@ -2,7 +2,7 @@ const User = require("../../../models/user");
 const jwt = require("jsonwebtoken");
 const activate_admin = async (req, res, next) => {
    jwt.verify(req.body.token, process.env.SECRET, async (err, decode) => {
-      if (err) return res.status(401).send({ status: "false" });
+      if (err) return res.status(401).send({ status: false });
       let user = await User.findOneAndUpdate(
          {
             confirmationCode: req.body.token,
