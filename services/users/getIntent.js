d096e89effd0ca6529d6b392f5e8ecfa327052f1
tawanchaiii <tawanchai_champ@hotmail.com>
2022-10-12 23:11:54 +0700

    change 30THB to 5000THB

--- services/users/getIntent.js
+++ services/users/getIntent.js
@@ -5,7 +5,7 @@ const calculateOrderAmount = (items) => {
    // Replace this constant with a calculation of the order's amount
    // Calculate the order total on the server to prevent
    // people from directly manipulating the amount on the client
-   return 3000;
+   return 500000;
 };
 
 const getIntent = async (req, res) => {
