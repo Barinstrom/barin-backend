7ea812e97600922b71a092746c7f64fc39c77b0f
tawanchaiii <tawanchai_champ@hotmail.com>
2022-11-09 13:01:24 +0700

    change limit to 19

--- services/users/system_admin/getApprovedSchool.js
+++ services/users/system_admin/getApprovedSchool.js
@@ -6,22 +6,32 @@ const getApprovedSchool = (req, res) => {
    if (req.query.query)
       tmp = new RegExp("(?!^all$)" + "(" + req.query.query + ")");
    const query = {
-      schoolID: { $regex: tmp, $options: "i" },
+      schoolID: {
+         $regex: tmp,
+         $options: "i"
+      },
       status: "approve",
       paymentStatus: "success",
    };
    const page = req.query.page || 1;
-   const limit = 1;
+   const limit = 10;
    const sort = "field -paymentDate";
 
    //{ color: "blue", published: true }, { page: 1, limit: 10, projection: { color: 1 } }
    schoolModel
-      .paginate(query, { page, limit, sort })
+      .paginate(query, {
+         page,
+         limit,
+         sort
+      })
       .then((result) => {
          res.send(result);
       })
       .catch((err) => {
-         res.json({ success: false, message: "update email or password fail" });
+         res.json({
+            success: false,
+            message: "update email or password fail"
+         });
          res.status(400).send("paginate error");
       });
 };
--- services/users/system_admin/getPendingSchool.js
+++ services/users/system_admin/getPendingSchool.js
@@ -6,17 +6,24 @@ const getPendingSchool = (req, res) => {
    if (req.query.query)
       tmp = new RegExp("(?!^all$)" + "(" + req.query.query + ")");
    const query = {
-      schoolID: { $regex: tmp, $options: "i" },
+      schoolID: {
+         $regex: tmp,
+         $options: "i"
+      },
       status: "pending",
       paymentStatus: "success",
    };
    const page = req.query.page || 1;
-   const limit = 3;
+   const limit = 10;
    const sort = "field paymentDate";
 
    //{ color: "blue", published: true }, { page: 1, limit: 10, projection: { color: 1 } }
    schoolModel
-      .paginate(query, { page, limit, sort })
+      .paginate(query, {
+         page,
+         limit,
+         sort
+      })
       .then((result) => {
          res.send(result);
       })
