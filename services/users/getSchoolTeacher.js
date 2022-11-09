34f220fea992947496f792407913d460e0a3cdec
tawanchaiii <tawanchai_champ@hotmail.com>
2022-11-09 11:16:23 +0700

    add email field on getSchool_Paginate

--- services/users/getSchoolTeacher.js
+++ services/users/getSchoolTeacher.js
@@ -1,110 +1,146 @@
 const userModel = require('../../models/user');
 
 const getSchoolTeacher = async (req, res) => {
-    const page = req.query.page || 1;
-    const limit = 5;
-    let tmp = ""
-    if (req.query.query)
-       tmp = new RegExp("^" + req.query.query );
+   const page = req.query.page || 1;
+   const limit = 5;
+   let tmp = ""
+   if (req.query.query)
+      tmp = new RegExp("^" + req.query.query);
 
-    let temp = await userModel
-       .aggregate([
-          {
-             $match:
-                // {role:"teacher"}
-                {
-                   $and: [
-                      { role: "teacher" },
-                      { schoolID: req.userInfo.schoolID },
-                   ],
-                },
-          },
-          {
-             $lookup: {
-                from: "teachers", // ตารางที่อยาก join
-                localField: "_id", // user
-                foreignField: "userID", // teacherID
-                as: "teacher", // ชื่อผลลัพท์
-             }, // [{... userData, teacher:{userID:[objectID]}},...]
-          },
-          { $unwind: "$teacher" },
-          {
-             $match: {
-                "teacher.firstname" : {$regex: tmp ,$options:'i'},
-             },
-          },
-          {
-            $count: "count"
-          }
-       ])
-       .exec();
-       /*[
+   let temp = await userModel
+      .aggregate([{
+            $match:
+            // {role:"teacher"}
             {
-                "count": 1
-            }
-         ] */
-
-    let count = temp[0].count;
-
-    const totalDocs = count;
-    const totalPages = Math.ceil( count/limit );
-    const pagingCounter = (page-1)*limit +1 ;
-    const hasPrevPage = page!=1;
-    const hasNextPage = page<totalPages;
-    let prevPage = null;
-    if (hasPrevPage){
-        prevPage = parseInt(page)-1;
-    }
-    let nextPage = null;
-    if(hasNextPage){
-        nextPage = parseInt(page)+1;
-    }
-
-    let _docs = await userModel
-    .aggregate([
-       {
-          $match:
-             // {role:"teacher"}
-             {
-                $and: [
-                   { role: "teacher" },
-                   { schoolID: req.userInfo.schoolID },
-                ],
-             },
-       },
-       {
-          $lookup: {
-             from: "teachers", // ตารางที่อยาก join
-             localField: "_id", // user
-             foreignField: "userID", // teacher
-             as: "teacher", // ชื่อผลลัพท์
-          }, // [{... userData, teacher:{userID:[objectID]}},...]
-       },
-       { $unwind: "$teacher" },
-       {
-          $match: {
-             "teacher.firstname" : {$regex: tmp ,$options:'i'},
-          },
-       },
-       { $sort: {
-        email:1
-        }},
+               $and: [{
+                     role: "teacher"
+                  },
+                  {
+                     schoolID: req.userInfo.schoolID
+                  },
+               ],
+            },
+         },
+         {
+            $lookup: {
+               from: "teachers", // ตารางที่อยาก join
+               localField: "_id", // user
+               foreignField: "userID", // teacherID
+               as: "teacher", // ชื่อผลลัพท์
+            }, // [{... userData, teacher:{userID:[objectID]}},...]
+         },
+         {
+            $unwind: "$teacher"
+         },
+         {
+            $match: {
+               "teacher.firstname": {
+                  $regex: tmp,
+                  $options: 'i'
+               },
+            },
+         },
+         {
+            $count: "count"
+         }
+      ])
+      .exec();
+   /*[
         {
-         $skip: limit*(page-1)
-        },
-       {
-        $limit: limit
-       }
-    ])
-    .exec();
+            "count": 1
+        }
+     ] */
+   let count;
+   if (temp[0]) {
+      count = temp[0].count;
+   } else {
+      count = 0;
+   }
 
-    let docs = []
-    for(let i=0;i<_docs.length;i++){
-        let tmp1 = _docs[i].teacher;
-        docs.push(tmp1)
-    }
 
-    res.send({docs,totalDocs,limit,totalPages,page,pagingCounter,hasPrevPage,hasNextPage,prevPage,nextPage});
+   const totalDocs = count;
+   const totalPages = Math.ceil(count / limit);
+   const pagingCounter = (page - 1) * limit + 1;
+   const hasPrevPage = page != 1;
+   const hasNextPage = page < totalPages;
+   let prevPage = null;
+   if (hasPrevPage) {
+      prevPage = parseInt(page) - 1;
+   }
+   let nextPage = null;
+   if (hasNextPage) {
+      nextPage = parseInt(page) + 1;
+   }
+
+   let _docs = await userModel
+      .aggregate([{
+            $match:
+            // {role:"teacher"}
+            {
+               $and: [{
+                     role: "teacher"
+                  },
+                  {
+                     schoolID: req.userInfo.schoolID
+                  },
+               ],
+            },
+         },
+         {
+            $lookup: {
+               from: "teachers", // ตารางที่อยาก join
+               localField: "_id", // user
+               foreignField: "userID", // teacher
+               as: "teacher", // ชื่อผลลัพท์
+            }, // [{... userData, teacher:{userID:[objectID]}},...]
+         },
+         {
+            $unwind: "$teacher"
+         },
+         {
+            $match: {
+               "teacher.firstname": {
+                  $regex: tmp,
+                  $options: 'i'
+               },
+            },
+         },
+         {
+            $sort: {
+               email: 1
+            }
+         },
+         {
+            $skip: limit * (page - 1)
+         },
+         {
+            $limit: limit
+         }
+      ])
+      .exec();
+
+   let docs = []
+   for (let i = 0; i < _docs.length; i++) {
+      let tmp1 = _docs[i].teacher;
+      let userId = tmp1.userID
+      const _user = await userModel.findById(userId).exec();
+      tmp1["email"] = _user.email
+
+      docs.push(tmp1)
+   }
+
+   res.send({
+      docs,
+      totalDocs,
+      limit,
+      totalPages,
+      page,
+      pagingCounter,
+      hasPrevPage,
+      hasNextPage,
+      prevPage,
+      nextPage
+   });
 }
 
 module.exports = getSchoolTeacher;
